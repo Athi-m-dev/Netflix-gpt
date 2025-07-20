466db959023c007ceef3089cde92976252c0bf2f
Athithiyan-arch <athithiyanm87@gmail.com>
2025-07-20 22:02:34 +0530

    Add Firebase Hosting GitHub Action

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAgYCrK_PmbAOqjVnOKDBTG_K3KrJ5Jq_0",
+  authDomain: "netflix-gpt-41f2a.firebaseapp.com",
+  projectId: "netflix-gpt-41f2a",
+  storageBucket: "netflix-gpt-41f2a.firebasestorage.app",
+  messagingSenderId: "970113711537",
+  appId: "1:970113711537:web:6afcd78ed242a3f0e31190",
+  measurementId: "G-BVLDSD21YB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
